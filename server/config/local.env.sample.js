e0e9dcd2ecdb4370b080f0e2db56de3f2a2c8462
Manbec <mbecerramarrufo@acm.org>
2016-02-22 14:43:25 -0600

    app keys

--- server/config/local.env.sample.js
+++ server/config/local.env.sample.js
@@ -6,18 +6,18 @@
 // You will need to set these on the server you deploy to.
 
 module.exports = {
-  DOMAIN:           'http://localhost:9000',
-  SESSION_SECRET:   'camarboard-secret',
+  DOMAIN: 'http://localhost:9000',
+  SESSION_SECRET: 'camarboard-secret',
 
-  FACEBOOK_ID:      'app-id',
-  FACEBOOK_SECRET:  'secret',
+  FACEBOOK_ID: '1041274819269014',
+  FACEBOOK_SECRET: '76b38a2bdb56acfd27512dc8a8a987c0',
 
-  TWITTER_ID:       'app-id',
-  TWITTER_SECRET:   'secret',
+  TWITTER_ID: 'OcWkjs0VHTTm5tq8JgoJrOCqL',
+  TWITTER_SECRET: '73S9JNIlnl3WzBGSeittxuwTUbzU5YbGfallCtxddYOm2TFiVE',
 
-  GOOGLE_ID:        'app-id',
-  GOOGLE_SECRET:    'secret',
-
+  GOOGLE_ID: '711553882268-1ddlfh93q3r4ltohtq0o6fcc5lidb2ot.apps.googleusercontent.com',
+  GOOGLE_SECRET: 'pXTZLpheGOnAb0f082wKbiXU',
+  
   // Control debug level for modules using visionmedia/debug
   DEBUG: ''
 };

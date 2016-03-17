5f1fe1e95651acc0f391ef36f032539a9972655f
Manbec <mbecerramarrufo@acm.org>
2016-03-17 13:36:48 -0600

    graphic editor

--- client/home/home.js
+++ client/home/home.js
@@ -159,8 +159,7 @@ $scope.connection.onmessage = function (message) {
 
                     var textAtributes = textC
                          .text(function (d) { return (d.capacity-d.full+d.intransit)+"/"+d.capacity; })
-                        .attr("x", function (d) { return d.x -d.w/2; })
-                        .attr("y", function (d) { return d.y + 15; })
+                        .attr("transform", function (d) { return "translate("+(d.x+d.w/2-20)+", "+d.y+") rotate("+d.angle+")"; })
                         .style('fill', 'black');
 
                     var rectanglesAtributes = rectangles
